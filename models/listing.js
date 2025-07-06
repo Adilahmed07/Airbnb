912e76a6064c8932a466210d98c38d0f5d11e16f
Adil Ahmed <adilahmed26179@gmail.com>
2025-07-07 02:17:18 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,8 @@
+const mongoose = require("mongoose"); // ✅ Required!
 const Schema = mongoose.Schema;
-const Review=require('./review.js')
+
+const Review = require('./review.js');
+
 const listingSchema = new Schema({
   title: {
     type: String,
@@ -7,28 +10,29 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    url:String,
-    filename:String,
+    url: String,
+    filename: String,
   },
   price: Number,
   location: String,
   country: String,
-  reviews:[{
-    type:Schema.Types.ObjectId,
-    ref:"Review"
-  }],
-  owner:{
-    type:Schema.Types.ObjectId,
-    ref:"User",
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review"
+    }
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
   }
 });
 
-
-listingSchema.post("findOneAndDelete",async(listing)=>{
-  if(listing){
-    await Review.deleteMany({_id:{$in:listing.reviews}})
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
   }
-})
+});
 
 const Listing = mongoose.model("Listing", listingSchema);
 
